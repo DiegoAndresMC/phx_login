5eef9548c1a09d11e5208ad6a8958f5da9ca794d
Diego <diego.andresmc@gmail.com>
2019-06-11 17:12:43 -0400

    Agregando navbar y modificando seguridad con comeoin

--- lib/login_phx/accounts/user.ex
+++ lib/login_phx/accounts/user.ex
@@ -1,20 +1,45 @@
 defmodule LoginPhx.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias LoginPhx.Accounts.{User, Encryption}
 
   schema "users" do
     field :email, :string
     field :password_hash, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
 
+  @required_fields ~w(email username password)
+  @optional_fields ~w()
+
   @doc false
-  def changeset(user, attrs) do
+  def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:username, :email, :password_hash])
-    |> validate_required([:username, :email, :password_hash])
+    |> cast(attrs, @required_fields, @optional_fields)
+    |> validate_required([:username, :email])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> encrypt_password
+  end
+
+  defp encrypt_password(changeset) do
+    password = get_change(changeset, :password)
+
+    if password do
+      encrypted_password = Encryption.hash_password(password)
+      put_change(changeset, :password_hash, encrypted_password)
+    else
+      changeset
+    end
+  end
+
+  defp downcase_email(changeset) do
+    update_change(changeset, :email, &String.downcase/1)
   end
 end
